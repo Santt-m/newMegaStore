f24e02508c35cf89d8f77159766c437313863bb1
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-11-08 17:03:14 -0300

    Vamos haciendo el Styling

--- js/header.js
+++ js/header.js
@@ -1,33 +1,4 @@
-const navBtn = document.getElementById('navBtn');
-const navMenu = document.querySelector('nav');
-
-const cartBtn = document.getElementById('cartBtn');
-const cartMenu = document.getElementById('cartMenu')
-
-export function init () {
-    console.log('header init')
-
-    // boton nav
-    navBtn.addEventListener('click', () =>{
-        // si el nav esta active
-        if(navMenu.classList.contains('fx-active')){
-            // activa efecto de cierre
-            navMenu.classList.add('fx-close');
-            // deja pasar el efecto y borra las class
-            setTimeout(()=> {
-                navMenu.classList.remove('fx-active','fx-close')
-            },500);
-        }
-        // si el nav no esta active
-        else{
-            // forzar cierre cart
-            cartMenu.classList.add('dnone')
-            // activa el nav
-            navMenu.classList.add('fx-active');
-        };
-
-    });
-};
-
-// Init al cargar el dom
-document.addEventListener('DOMContentLoaded', init);
+document.querySelector('#btnMenu').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#navbar').classList.toggle('active')
+})
